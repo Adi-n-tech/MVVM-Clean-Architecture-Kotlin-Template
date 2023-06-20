46addaf2a2417b5b28956cc2f3971718fe22a37d
Aditya Nandardhane <adityanandardhane06@gmail.com>
2023-06-20 17:19:13 +0530

    optimize network call code

--- app/src/main/java/com/app/template/viewmodel/dashboard/repository/DashboardRepository.kt
+++ app/src/main/java/com/app/template/viewmodel/dashboard/repository/DashboardRepository.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.app.template.model.CommonResponse
 import com.app.template.utils.network.NetworkResult
+import com.app.template.utils.network.handleApiResponse
 import com.app.template.viewmodel.dashboard.api.DashboardService
 import com.google.gson.JsonObject
 import javax.inject.Inject
@@ -19,18 +20,9 @@ class DashboardRepository @Inject constructor(
 
 
     suspend fun getApiConstants() {
-        _apiConstants.postValue(NetworkResult.Loading())
-        //-----
        /* val jsonObject = JsonObject()
         jsonObject.addProperty("userId", userId)
         jsonObject.addProperty("apiToken", "dnnksd")*/
-        val response = dashboardService.getApiConstants()
-        if (response.isSuccessful && response.body() != null) {
-            _apiConstants.postValue(NetworkResult.Success(response.body()!!))
-        } else if (response.errorBody() != null) {
-            _apiConstants.postValue(NetworkResult.Error(response.errorBody().toString()))
-        } else {
-            _apiConstants.postValue(NetworkResult.Error("Something went wrong !!"))
-        }
+        handleApiResponse({ dashboardService.getApiConstants() }, _apiConstants)
     }
 }
